9e83ed9404fd0df7e70f6fc948d49bc33b982d7c
elcapo081011-byte <elcapo081011@gmail.com>
2026-09-17 00:05:47 -0400

    Add files via upload

---
+++ apps/backend/src/olt/olt.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { OltController } from './olt.controller';
+import { OltService } from './olt.service';
+import { GenericSnmpOltProvider } from './generic-snmp-olt.provider';
+import { PrismaService } from '../common/prisma/prisma.service';
+import { AuditService } from '../common/audit/audit.service';
+
+@Module({
+  controllers: [OltController],
+  providers: [OltService, GenericSnmpOltProvider, PrismaService, AuditService],
+  exports: [OltService],
+})
+export class OltModule {}
